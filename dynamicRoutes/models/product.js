5243771ca9ed6c0a8cb1a6997ce8f851960bf309
Huỳnh Hữu Khánh <khanh201011@gmail.com>
2021-07-29 15:22:24 +0700

    Continue

--- dynamicRoutes/models/product.js
+++ dynamicRoutes/models/product.js
@@ -9,20 +9,29 @@ const p = path.join(
 
 const getProductsFromFile = (cb) => {
   fs.readFile(p, (err, fileContent) => {
-    if (err) cb([]);
-    else cb(JSON.parse(fileContent));
+    if (err) {
+      cb([]);
+    } else {
+      cb(JSON.parse(fileContent));
+    }
   });
 };
 
 module.exports = class Product {
-  constructor(title) {
+  constructor(title, imageUrl, description, price) {
     this.title = title;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this.price = price;
   }
 
   save() {
+    this.id = Math.random().toString();
     getProductsFromFile((products) => {
       products.push(this);
-      fs.writeFile(p, JSON.stringify(products), (err) => {});
+      fs.writeFile(p, JSON.stringify(products), (err) => {
+        console.log(err);
+      });
     });
   }
 
